36d624a396d87d9485c5c7dd6c6a6ce8e0508e31
vituhonorato <vituhonorato.89@gmail.com>
2022-06-06 16:35:45 -0300

    criação de calculadora simples em react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+
+  //Lista de states da calcreact:
+  const [valorTela,setValorTela]=useState('')
+  const [resultado,setResultado]=useState(0)
+  const [acumulador,setAcumulador]=useState(0)
+  const [operado,setOperado]=useState(false)
+
+  //Lista de Componentes da calcreact
+
+  //Componente tela :
+  const Tela = (valor,res)=>{
+    return(
+      <div style={cssTela}>
+        <span style={cssTelaOper}>{valor}</span>
+        <span style={cssTelaRes}>{res}</span>
+      </div>
+    )
+
+  }
+  //Componente botão:
+  const Btn = (label,onClick)=>{
+    return(
+      <button style={cssBtn} onClick={onClick}>{label}</button>
+
+    )
+  }
+  //FUNÇÕES:
+
+  //Função pra o que for digitado na tela:
+  const addDigitoTela=(d)=>{
+    if((d=='+' || d=='-' || d=='*' || d=='/ ') && operado){
+      console.log('+-*/')
+      setOperado(false)
+      setValorTela(resultado+d)
+      return
+    }
+    if (operado){
+      setValorTela(d)
+      setOperado(false)
+      return
+    } 
+    const valorDigitadoTela=valorTela+d
+    setValorTela(valorDigitadoTela)
+    
+  }
+
+  //Função pra limpar a memoria:
+  const limparMemoria=()=>{
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
+    setAcumulador(0)
+    return
+
+  }
+
+  //Função de operação faz o calculo de tudo que está na tela:
+  const Operacao=(oper)=>{
+      if(oper=='bs'){
+        let vtela=valorTela
+        vtela=vtela.substring(0,(vtela.length-1))
+        setValorTela(vtela)
+        setOperado(false)
+        return
+      }
+       try{
+         const r=eval(valorTela)
+         setAcumulador(r)
+         setResultado(r)
+         setOperado(true)
+       }catch{
+         setResultado('ERRO')
+       }
+
+  }
+
+  //Estilos
+  const cssContainer={
+    display:'flex',
+    justifyContent:"flex-start",
+    alignItems:'center',
+    flexDirection:'column',
+    width:300,
+    border:'1px solid #000'
+
+  }
+
+  const cssBotoes={
+      flexDirection:'row',
+      flexWrap:'wrap'
+  }
+
+
+
+  const cssTela={
+    display: 'flex',
+    paddingLeft:20,
+    paddingRight:20,
+    justifyContent:"center",
+    alignItems:'flex-start',
+    backgroundColor: '#444',
+    flexDirection: 'column',
+    width: 260
+  }
+  
+  const cssTelaOper={
+    fontSize:25,
+    color:'#fff',
+    height:'20'
+   }
+
+  const cssTelaRes={
+    fontSize:50,
+    color:'#fff',
+   }
+  const cssBtn={
+    fontSize:30,
+    width:75,
+    height:75,
+    padding:20,
+    backgroundColor:'#000',
+    color:'#fff',
+    borderColor:"#000",
+    textAlign:"center",
+    outline:'none'
+
+  }
+  
+  return(
+    <>
+    <div style={cssContainer}>
+      <h3>Calculadora matemática simples</h3>
+      {Tela(valorTela,resultado)}
+      <div style={cssBotoes}>
+        {Btn('AC',limparMemoria)}
+        {Btn('(',()=>addDigitoTela('(') )}
+        {Btn(')',()=>addDigitoTela(')') )}
+        {Btn('/',()=>addDigitoTela('/') )}
+        {Btn('7',()=>addDigitoTela('7') )}
+        {Btn('8',()=>addDigitoTela('8') )}
+        {Btn('9',()=>addDigitoTela('9') )}
+        {Btn('*',()=>addDigitoTela('*') )}
+        {Btn('4',()=>addDigitoTela('4') )}
+        {Btn('5',()=>addDigitoTela('5') )}
+        {Btn('6',()=>addDigitoTela('6') )}
+        {Btn('-',()=>addDigitoTela('-') )}
+        {Btn('1',()=>addDigitoTela('1') )}
+        {Btn('2',()=>addDigitoTela('2') )}
+        {Btn('3',()=>addDigitoTela('3') )}
+        {Btn('+',()=>addDigitoTela('+') )}
+        {Btn('0',()=>addDigitoTela('0') )}
+        {Btn('.',()=>addDigitoTela('.') )}
+        {Btn('<-',()=>Operacao('bs') )}
+        {Btn('=',()=>Operacao('=') )}
+        
+
+
+
+
+
+
+
+
+
+      </div>
+
     </div>
+
+
+    </>
   );
 }
-
-export default App;
